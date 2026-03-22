b64624af04b5ff8af493f6b635c2ad73d5b51fc5
Tyler Long <aethdae@gmail.com>
2026-03-21 20:45:33 -0500

    refine battle logic, add floating damage numbers

--- BattleHandler.js
+++ BattleHandler.js
@@ -1,5 +1,13 @@
 const drag = document.getElementById("dragon");
 const warrior = document.getElementById("warrior");
+const battleArea = document.getElementById("battleArea");
+const damageNumClasses = [
+  "p-1",
+  "bg-gray-200/80",
+  "rounded-md",
+  "border-2",
+  "border-black",
+];
 
 let isDragTurn = true;
 
@@ -9,6 +17,7 @@ export function doBattle() {
     isDragTurn = false;
     const dragList = drag.className;
     drag.classList = warrior.classList;
+    displayDamage(warrior);
     setTimeout(() => {
       drag.className = dragList;
     }, width / 2);
@@ -16,6 +25,7 @@ export function doBattle() {
     isDragTurn = true;
     const warriorList = warrior.className;
     warrior.classList = drag.classList;
+    displayDamage(drag);
     setTimeout(() => {
       warrior.className = warriorList;
     }, width / 1.5);
@@ -26,4 +36,33 @@ export function startBattle() {
   setInterval(() => {
     doBattle();
   }, 3500);
+}
+
+function displayDamage(damageReciever) {
+  const damageNum = document.createElement("p");
+  damageNum.innerText = getDamageNum();
+  damageNum.classList = damageReciever.classList;
+  damageNum.classList.add(...damageNumClasses);
+  if (isDragTurn) {
+    setTimeout(() => {
+      damageNum.classList.remove("top-[2%]");
+      damageNum.classList.add("top-[-8%]");
+      setTimeout(() => {
+        battleArea.removeChild(damageNum);
+      }, 1000);
+    }, 1);
+  } else {
+    setTimeout(() => {
+      damageNum.classList.remove("top-[50%]");
+      damageNum.classList.add("top-[40%]");
+      setTimeout(() => {
+        battleArea.removeChild(damageNum);
+      }, 1000);
+    }, 1);
+  }
+  damageNum.style.zIndex = 2;
+  battleArea.appendChild(damageNum);
+}
+function getDamageNum() {
+  return Math.floor(Math.random() * 90);
 }
